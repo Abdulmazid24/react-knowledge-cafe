56cded21d2a5546ab651197bcf24a5566bbb75e1
Abdulmazid24 <a.m.abdulmazid24@gmail.com>
2024-10-24 17:00:51 +0600

    navbar and some blogs add

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,22 @@
 import PropTypes from 'prop-types';
 const Blog = ({ blog }) => {
+  const { title, cover_img, reading_time, author, author_img } = blog;
   console.log(blog);
-  return <div></div>;
+  return (
+    <div>
+      <img src={cover_img} alt={`cover picture of the title ${title}`} />
+      <div className="flex justify-between">
+        <div>
+          <img className="w-10 h-10" src={author_img} alt="" />
+          <p>{author}</p>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+    </div>
+  );
 };
 
 Blog.propTypes = {
